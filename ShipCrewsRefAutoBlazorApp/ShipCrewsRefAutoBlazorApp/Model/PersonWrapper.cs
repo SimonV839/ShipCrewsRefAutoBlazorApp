8a1bc5a3e2cf31ee2e51ebbbd70ac890f5945cad
SimonV839 <167682180+SimonV839@users.noreply.github.com>
2024-07-13 09:17:11 +0100

    new person using personwrapper but not yet using the property validations.

---
+++ ShipCrewsRefAutoBlazorApp/ShipCrewsRefAutoBlazorApp/Model/PersonWrapper.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShipCrewsRefAutoBlazorApp.Model
+{
+    public class PersonWrapper
+    {
+        public int PersonId
+        {
+            get => servicePerson.PersonId;
+            set => servicePerson.PersonId = value;
+        }
+
+        [Required, MinLength(2), MaxLength(50)]
+        public string FirstName
+        {
+            get => servicePerson.FirstName;
+            set => servicePerson.FirstName = value;
+        }
+
+        [Required, MinLength(2), MaxLength(50)]
+        public string LastName
+        {
+            get => servicePerson.LastName;
+            set => servicePerson.LastName = value;
+        }
+
+        [Required, Range(1, 3, ErrorMessage = "Please enter a value between 1 and 3 inclusive.")]
+        public int RoleId
+
+        {
+            get => servicePerson.RoleId ?? -1;
+            set => servicePerson.RoleId = value;
+        }
+
+        public PersonHacked PersonHacked
+        {
+            get => servicePerson;
+            set => servicePerson = value;
+        }
+
+        private PersonHacked servicePerson = new PersonHacked();
+    }
+}
